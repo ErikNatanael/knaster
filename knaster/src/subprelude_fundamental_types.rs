1cbf7bc84c9788878b47254399343d56a7fc3ca4
Erik Natanael Gustafsson <erik@eriknatanael.com>
2025-02-20 18:25:43 +0100

    More types in preludes

--- knaster/src/subprelude_fundamental_types.rs
+++ knaster/src/subprelude_fundamental_types.rs
@@ -1,8 +1,13 @@
 // Fundamental types
 #[allow(unused)]
 pub use knaster_graph::{
-    audio_backend::AudioBackend, handle::HandleTrait, numeric_array::NumericArray, runner::Runner,
-    runner::RunnerOptions, typenum::*, wrappers_core::UGenWrapperCoreExt,
-    wrappers_graph::done::WrDone, Done, Float, Frame, ParameterRange, ParameterType,
-    ParameterValue, Size, UGen,
+    audio_backend::AudioBackend, connectable::Connectable, graph::GraphOptions,
+    handle::HandleTrait, numeric_array::NumericArray, runner::Runner, runner::RunnerOptions,
+    typenum::*, wrappers_core::UGenWrapperCoreExt, wrappers_graph::done::WrDone, Beats, Done,
+    Float, Frame, ParameterRange, ParameterType, ParameterValue, Seconds, Size, UGen,
 };
+
+#[cfg(feature = "cpal")]
+pub use knaster_graph::audio_backend::cpal::{CpalBackend, CpalBackendOptions};
+#[cfg(feature = "jack")]
+pub use knaster_graph::audio_backend::jack::JackBackend;
