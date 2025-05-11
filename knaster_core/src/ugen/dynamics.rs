decaeaac856cf6276da5034e381241a8f95aeaa5
Erik Natanael Gustafsson <erik@eriknatanael.com>
2025-05-11 14:55:08 +0200

    Safety limiter

---
+++ knaster_core/src/ugen/dynamics.rs
@@ -0,0 +1,26 @@
+use core::marker::PhantomData;
+
+use knaster_primitives::Float;
+
+pub struct SafetyLimiter<F: Float> {
+    _float: PhantomData<F>,
+}
+#[knaster_macros::ugen]
+impl<F: Float> SafetyLimiter<F> {
+    pub fn new() -> Self {
+        Self {
+            _float: PhantomData,
+        }
+    }
+    fn process(
+        &mut self,
+        _ctx: &mut super::AudioCtx,
+        _flags: &mut super::UGenFlags,
+        input: [F; 1],
+    ) -> [F; 1] {
+        let s = input[0];
+        let s = s.clamp(F::new(-1.0), F::new(1.0));
+        let s = if s.is_nan() { F::new(0.0) } else { s };
+        [s]
+    }
+}
